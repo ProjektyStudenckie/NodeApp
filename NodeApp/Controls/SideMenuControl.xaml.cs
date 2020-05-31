47eff73a8d55df6f336ffbc88e43242a611d309f
SosnaK1 <jakub.sosna@icloud.com>
2020-05-31 23:28:29 +0200

    Create SideMenu Base

---
+++ NodeApp/Controls/SideMenuControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace NodeApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SideMenuControl.xaml
+    /// </summary>
+    public partial class SideMenuControl : UserControl
+    {
+        public SideMenuControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
